635c1c4b6acc6b75a0f1c5afe0c66503baac41ce
masaaki-shibata <69470093+masaaki-shibata@users.noreply.github.com>
2020-08-24 21:59:28 +0900

    チャットルーム消去機能の実装とエラーの戦い

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,10 +8,10 @@ class MessagesController < ApplicationController
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-    if @message = save
+    if @message.save
       redirect_to room_messages_path(@room)
     else
-      @messages = @room.messages.inclides(:user)
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
